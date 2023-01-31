1fa58e8bacc8408f0146db67932081600f195910
Amjad Khaliliah <amjadkhaliliah1998@gmail.com>
2023-01-31 16:25:50 +0200

    Initial commit

---
+++ src/main/java/com/amjadprojects/course/trainingcourse/TrainingCourseApplication.java
@@ -0,0 +1,13 @@
+package com.amjadprojects.course.trainingcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amjadprojects/course/trainingcourse/TrainingCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amjadprojects.course.trainingcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
